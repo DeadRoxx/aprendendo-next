5b332146eeddba711fd238aaeef8a742cbda2923
Eduardo Rodrigues <eduardorodrigues94@hotmail.com>
2021-01-20 10:41:05 -0300

    Environment Variable

--- pages/api/time.tsx
+++ pages/api/time.tsx
@@ -1,5 +1,9 @@
 const time = (request: any, response: any) => {
   const dynamicDate = new Date();
-  response.json({ date: dynamicDate.toLocaleString() })
+
+  response.json({
+    date: dynamicDate.toLocaleString(),
+    secret: process.env.CONVERTKIT_API_SECRET
+  })
 }
 export default time;
