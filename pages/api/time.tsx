6628e5574813fd7301b1bcd1c1697c7c6057c02d
Eduardo Rodrigues <eduardorodrigues94@hotmail.com>
2021-01-20 10:55:03 -0300

    using cache

--- pages/api/time.tsx
+++ pages/api/time.tsx
@@ -1,9 +1,11 @@
 const time = (request: any, response: any) => {
   const dynamicDate = new Date();
 
+  response.setHeader('Cache-Control', 's-maxage=10, stale-while-revalidate');
+
   response.json({
     date: dynamicDate.toLocaleString(),
-    secret: process.env.CONVERTKIT_API_SECRET
+    secret: process.env.CONVERTKIT_API_SECRET + 2
   })
 }
 export default time;
