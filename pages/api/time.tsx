af7e10091d5ded416c7246d4c9765b68a769844e
Eduardo Rodrigues <eduardorodrigues94@hotmail.com>
2021-01-20 10:31:38 -0300

    creating api

--- pages/api/time.tsx
+++ pages/api/time.tsx
@@ -1,10 +1,5 @@
-
 const time = (request: any, response: any) => {
   const dynamicDate = new Date();
-
-  response.json({
-    date: dynamicDate.toLocaleString()
-  })
+  response.json({ date: dynamicDate.toLocaleString() })
 }
-
 export default time;
